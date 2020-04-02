a38ed3a4f3f69310177e6fbae8b0317b9141a916
Carlos Andrés Miranda Méndez <62572969+CarlosMM28@users.noreply.github.com>
2020-04-02 15:06:01 -0600

    Update PRUEBA30DEMARZO.java

--- PRUEBA30DEMARZO/src/prueba30demarzo/PRUEBA30DEMARZO.java
+++ PRUEBA30DEMARZO/src/prueba30demarzo/PRUEBA30DEMARZO.java
@@ -15,6 +15,11 @@ public class PRUEBA30DEMARZO {
         System.out.println("Abner Obed Dominguez Rodriguez");
         System.out.println("Colonia El Oasis, Zona 8, Huehuetenango");
         System.out.println("42252923");
+        
+        System.out.println("Carlos Andrés Miranda Méndez");
+        System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
+        System.out.println("58385781");
+
     }
     
 }
