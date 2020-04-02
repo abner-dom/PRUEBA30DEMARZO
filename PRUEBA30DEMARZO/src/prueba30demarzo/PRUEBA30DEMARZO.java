15c68d0ad45d187ed97fc6b6c26312c092c0cbb2
Usuario <Usuario@192.168.1.71>
2020-04-02 14:33:44 -0600

    Esto es el programa base hecho por mi: Denis Joel Cifuentes Cano

---
+++ PRUEBA30DEMARZO/src/prueba30demarzo/PRUEBA30DEMARZO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba30demarzo;
+
+/**
+ *
+ * @author Abner_Dominguez
+ */
+public class PRUEBA30DEMARZO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Abner Obed Dominguez Rodriguez");
+        System.out.println("Colonia El Oasis, Zona 8, Huehuetenango");
+        System.out.println("42252923");
+    }
+    
+}
